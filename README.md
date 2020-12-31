7f08470b1ce1fbdd7f9eb1d9ea6d88fd2cb12b75
Mohamed Arafa <mohamed.arafa@sugarcrm.com>
2020-12-30 19:03:13 -0500

    user-data and docs

--- README.md
+++ README.md
@@ -9,5 +9,10 @@ demo of ansible-pull on AWS
 
 ## Using
 - pre-requirements: ansible and git
-- modify group_vars/all.yaml to point to your liking. it includes variables like `git_repo` and `git_dir` 
+- modify group_vars/all.yaml to point to your liking. it includes variables like `git_repo` and `git_dir`
 - set up a new automation user in github and give this new user "write" access thru. see this github issue for [more info](https://github.com/jollygoodcode/jollygoodcode.github.io/issues/11)
+- save a SecureString parameter in `AWS Systems Manager / Parameter Store`. This demo uses the name `ansible-pull/git_token`
+
+## TODO
+- proof of concept that ansible-pull can work on a private repo
+- proof of concept that ansible-pull can call an ansible playbook (since ansible-pull itself is limited)
---
+++ user-data.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# install dependencies
+# this is how we will retrieve the git token from parameter store
+if grep -qi centos /etc/system-release # <- it says centos
+then
+	installer=yum
+	yum -y install unzip \
+	&& curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" \
+	&& unzip awscliv2.zip \
+	&& sudo ./aws/install
+fi
+if grep -qi ubuntu /etc/system-release # <- it says ubuntu
+then
+	installer=apt-get
+	curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" \
+	&& unzip awscliv2.zip \
+	&& sudo ./aws/install
+fi
+
+$installer -y install git
+$installer -y install ansible
+[ $? -eq 0 ] || amazon-linux-extras install ansible2
+
+# lets get the token from the parameter store
+region=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/.$//')
+parameter_name='/ansible-pull/git_token' # the parameter store was saved as a SecureString
+git_token=$(aws ssm get-parameters --names $parameter_name --with-decryption --region $region --query "Parameters[].Value"| sed -e 's/\[//g' -e s'/\]//g' -e 's/"//g')
+
+# and finally run ansible-pull for the very first time
+if ! [ -z $git_token ]
+then
+	sudo ansible-pull --directory /var/lib/ansible/local --url https://github.com/marafa/ansible-pull-demo.git --only-if-changed --inventory /var/lib/ansible/local/hosts -l 127.0.0.1
+else
+	sudo ansible-pull --directory /var/lib/ansible/local --url https://github.com/marafa/ansible-pull-demo.git --only-if-changed --inventory /var/lib/ansible/local/hosts -l 127.0.0.1
+fi
